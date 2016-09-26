95406a3cf18982ca3967cb165e161e55557e149a
Szép Norbert <szepnorbee@gmail.com>
2016-09-26 19:45:49 +0200

    Added EEPROM handling

--- menu_test_4/menu_test_4.ino
+++ menu_test_4/menu_test_4.ino
@@ -1,6 +1,10 @@
 #include <SPI.h>
+#include <EEPROMex.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 #include <menu.h>//menu macros and objects
 #include <genericKeyboard.h>
+#include <menuFields.h>
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7735.h> // Hardware-specific library
 #include <menuGFX.h>
@@ -18,61 +22,200 @@
 //-------VARS--------
 int old_button;
 bool runMenu=false;
+bool screenCleared = 0;
 
 byte futesStart = 2;
 byte futesStop = 60;
 byte tartasStart = 2;
-byte tartasStop = 150;
-
+byte tartasStop = 30;
 byte temperature = 70;
 byte histeresis = 2;
 
 byte menuTimeout = 60;
+byte activeProfil = 1;
+//-------EEPROM Adresses------------
+byte addrfStart1 = 10;
+byte addrfStop1 = 20;
+byte addrtStart1 = 30;
+byte addrtStop1 = 40;
+byte addrTemp1 = 50;
+byte addrHis1 = 60;
+byte addrMenuhide = 70;
+byte addrProfil = 80;
+
+byte addrfStart2 = 90;
+byte addrfStop2 = 100;
+byte addrtStart2 = 110;
+byte addrtStop2 = 120;
+byte addrTemp2 = 130;
+byte addrHis2 = 140;
+
+byte addrfStart3 = 150;
+byte addrfStop3 = 160;
+byte addrtStart3 = 170;
+byte addrtStop3 = 180;
+byte addrTemp3 = 190;
+byte addrHis3 = 200;
+
 //-------------------
 
 Adafruit_ST7735 tft(tftCS, dc, rst);
 menuGFX gfx(tft);
+
+/* Colors: BLACK, WHITE, RED, YELLOW, CYAN, MAGENTA, BRIGHT_RED, LIGHT_GREY, 
+DARK_GREY */
 
 //aux function
 bool nothing() {}
 
 bool pauseMenu() {
   runMenu=false;
+  screenCleared = 0;
+  return true;
+  }
+
+bool loadProfil1() {
+ activeProfil = 1;
+ EEPROM.write(addrProfil,activeProfil);
+ futesStart = EEPROM.read(addrfStart1);
+ futesStop = EEPROM.read(addrfStop1);
+ tartasStart = EEPROM.read(addrtStart1);
+ tartasStop = EEPROM.read(addrtStop1);
+ temperature = EEPROM.read(addrTemp1);
+ histeresis = EEPROM.read(addrHis1);
+ return true;
+}
+bool loadProfil2() {
+ activeProfil = 2;
+ EEPROM.write(addrProfil,activeProfil);
+ futesStart = EEPROM.read(addrfStart2);
+ futesStop = EEPROM.read(addrfStop2);
+ tartasStart = EEPROM.read(addrtStart2);
+ tartasStop = EEPROM.read(addrtStop2);
+ temperature = EEPROM.read(addrTemp2);
+ histeresis = EEPROM.read(addrHis2);
+ return true;
+}
+bool loadProfil3() {
+ activeProfil = 3;
+ EEPROM.write(addrProfil,activeProfil);
+ futesStart = EEPROM.read(addrfStart3);
+ futesStop = EEPROM.read(addrfStop3);
+ tartasStart = EEPROM.read(addrtStart3);
+ tartasStop = EEPROM.read(addrtStop3);
+ temperature = EEPROM.read(addrTemp3);
+ histeresis = EEPROM.read(addrHis3);
+ return true;
+}
+
+//---------Functions---------
+void mainScreen() {
+  runMenu=false;
+  tft.setTextColor(ST7735_GREEN,ST7735_BLACK);
+  tft.setCursor(0, 0); 
+  tft.print("Profil: ");
+  tft.println(activeProfil);
+  tft.setCursor(0, 20); 
+  tft.print("fSta: ");
+  tft.println(futesStart);
+  tft.setCursor(0, 40); 
+  tft.print("fSto: ");
+  tft.println(futesStop);
+  tft.setCursor(0, 60); 
+  tft.print("tSto: ");
+  tft.println(tartasStart);
+  tft.setCursor(0, 80); 
+  tft.print("tSto: ");
+  tft.println(tartasStop);
+}
+
+bool saveProfil1() {
+  EEPROM.write(addrProfil,activeProfil);
+  EEPROM.write(addrMenuhide,menuTimeout);
+  EEPROM.write(addrfStart1,futesStart);
+  EEPROM.write(addrfStop1,futesStop);
+  EEPROM.write(addrtStart1,tartasStart);
+  EEPROM.write(addrtStop1,tartasStop);
+  EEPROM.write(addrTemp1,temperature);
+  EEPROM.write(addrHis1,histeresis);
+  return true;
+  }
+
+bool saveProfil2() {
+  EEPROM.write(addrProfil,activeProfil);
+  EEPROM.write(addrMenuhide,menuTimeout);
+  EEPROM.write(addrfStart2,futesStart);
+  EEPROM.write(addrfStop2,futesStop);
+  EEPROM.write(addrtStart2,tartasStart);
+  EEPROM.write(addrtStop2,tartasStop);
+  EEPROM.write(addrTemp2,temperature);
+  EEPROM.write(addrHis2,histeresis);
+  return true;
+  }
+
+bool saveProfil3() {
+  EEPROM.write(addrProfil,activeProfil);
+  EEPROM.write(addrMenuhide,menuTimeout);
+  EEPROM.write(addrfStart3,futesStart);
+  EEPROM.write(addrfStop3,futesStop);
+  EEPROM.write(addrtStart3,tartasStart);
+  EEPROM.write(addrtStop3,tartasStop);
+  EEPROM.write(addrTemp3,temperature);
+  EEPROM.write(addrHis3,histeresis);
   return true;
   }
 
 //---------MENU TREE---------
 
-CHOOSE(temperature,temp,"Homerseklet",
-    VALUE("60",60,updTemp),
-    VALUE("65",65,updTemp),
-    VALUE("70",70,updTemp),
-    VALUE("75",75,updTemp),
-    VALUE("80",80,updTemp)
+CHOOSE(temperature,temp,"Temp",
+    VALUE("60",60,nothing),
+    VALUE("65",65,nothing),
+    VALUE("70",70,nothing),
+    VALUE("75",75,nothing),
+    VALUE("80",80,nothing)
 );
 
-CHOOSE(histeresis,temp,"Histeresis",
-    VALUE("2",2,updTemp),
-    VALUE("4",4,updTemp),
-    VALUE("6",6,updTemp)
+CHOOSE(histeresis,hister,"Toler.",
+    VALUE("2",2,nothing),
+    VALUE("4",4,nothing),
+    VALUE("6",6,nothing)
 );   
 
 MENU(timerMenu,"Idozites",
-    FIELD(futesStart,"Futes Start","s",1,20,1,1,nothing),
-    FIELD(futesStop,"Futes Stop","s",1,200,2,1,nothing),
-    FIELD(tartasStart,"Tartas Start","s",1,20,1,1,updTimer),
-    FIELD(tartasStop,"Tartas Stop","s",1,500,10,1,updTimer)
+    FIELD(futesStart,"fStart","s",1,20,1,1,nothing),
+    FIELD(futesStop,"fStop","s",1,200,10,1,nothing),
+    FIELD(tartasStart,"tStart","s",1,20,1,1,nothing),
+    FIELD(tartasStop,"tStop","p",1,30,10,1,nothing)
+);
+
+MENU(loadProfil,"Profilok",
+    OP("Profil 1",loadProfil1),
+    OP("Profil 2",loadProfil2),
+    OP("Profil 3",loadProfil3)
+);
+
+MENU(saveProfil,"Mentes",
+    OP("Profil 1",saveProfil1),
+    OP("Profil 2",saveProfil2),
+    OP("Profil 3",saveProfil3)
 );
 
 MENU(options,"Beallitasok",
-    OP("MenuTimer",updOptions),
-    OP("Frissit seb",updOptions),
-    OP("Mentes",updOptions)
+    SUBMENU(loadProfil),
+    OP("MenuTimer",nothing),
+    OP("Frissit seb",nothing),
+    SUBMENU(saveProfil),
+    OP("Firmware",nothing)
+);
+
+MENU(subMenu,"Homerseklet",
+    SUBMENU(temp),
+    SUBMENU(hister)
 );
 
 MENU(mainMenu,"Main menu",
     SUBMENU(timerMenu),
-    SUBMENU(temp),
+    SUBMENU(subMenu),
     SUBMENU(options),
     OP("Exit",pauseMenu)
 );
@@ -121,18 +264,6 @@ int getButton() {
   return button;
 }
 
-void mainScreen() {
-  runMenu=false;
-  tft.fillScreen(BLACK); 
-}
-
-void updTimer() {}
-
-void updTemp() {}
-
-void updOptions() {}
-
-void saveEEPROM() {}
 
 void setup() {
   Serial.begin(9600);
@@ -144,19 +275,30 @@ void setup() {
   tft.setTextSize(2);
   gfx.resX*=2;//update resolution after font size change
   gfx.resY*=2;//update resolution after font size change
-  gfx.maxX=12;
-  gfx.maxY=6;
-  gfx.bgColor=SILVER;
-  tft.fillScreen(GREEN);
-
+  gfx.maxX=13;
+  gfx.maxY=7;
+  gfx.bgColor=RGB565(125,125,125);
+  tft.fillScreen(BLACK);
+  menuTimeout = EEPROM.read(addrMenuhide);
+  activeProfil = EEPROM.read(addrProfil);
+  if (activeProfil == 1 ) loadProfil1();
+  if (activeProfil == 2 ) loadProfil2();
+  if (activeProfil == 3 ) loadProfil3();
 }
 
 void loop() {
-  if (runMenu) mainMenu.poll(gfx,mykb);
-  else if (readKeyboard()==menu::enterCode) {
+  if (runMenu) { 
+    mainMenu.poll(gfx,mykb);
+  }  else if (readKeyboard()==menu::enterCode) {
     runMenu=true;
+    tft.setTextColor(ST7735_RED,ST7735_BLACK);
     mainMenu.redraw(gfx,mykb);
+    mainMenu.poll(gfx,mykb);
   } else {
+    if (screenCleared == 0) {
+      tft.fillScreen(BLACK); 
+      screenCleared = 1;
+    }
     mainScreen();
   }
   
